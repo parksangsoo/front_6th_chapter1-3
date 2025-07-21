9315e3ed2584c57c9010a4077417a6116c3b62dd
pssw0728 <37687621+parksangsoo@users.noreply.github.com>
2025-07-21 16:27:26 +0900

    hooks 직접 만들어보기 useRef useMemo

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -2,5 +2,6 @@ import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
+  console.log(equals);
   return Component;
 }
